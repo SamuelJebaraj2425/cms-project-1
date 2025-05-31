f61bd4ef2d2a813ea2276be5338cafc67b90005a
pradeep4002 <pradheep1606@gmail.com>
2025-05-31 12:45:20 +0530

    second commit

--- client/src/pages/dashboard/DisplayAll.jsx
+++ client/src/pages/dashboard/DisplayAll.jsx
@@ -6,7 +6,7 @@ export default function DisplayAll() {
 	const { items } = useContext(StorageContext); // Access function from context
 
 	return (
-		<div className="flex flex-wrap gap-5">
+		<div className="flex flex-wrap gap-5 min-w-screen min-h-screen p-4 box-border w-screen">
 			{items.map((value, index) => {
 				return <Card key={index} item={value} index={index} />;
 			})}
